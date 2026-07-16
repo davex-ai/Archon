b00f41702f0be6743e1680fd1eb283350b3ee7d8
davex-ai <omotoyedave4@gmail.com>
2026-07-16 16:18:46 +0100

    Update layout.tsxxxx

--- archorn-client/src/app/layout.tsx
+++ archorn-client/src/app/layout.tsx
@@ -31,7 +31,7 @@ export default function RootLayout({
       <head>
         {/* We place the Metrixis tracker here inside the <head> */}
         <Script
-          src="https://localhost:8000/tracker.js"
+          src="http://127.0.0.1:8000/tracker.js"
           data-tracking-key="mtx_329dc8018b8f4a299c43b25ab413fd12"
           strategy="afterInteractive"
         />
